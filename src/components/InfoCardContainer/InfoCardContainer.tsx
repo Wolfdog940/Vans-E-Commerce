dd03a948a66f8f651e43cb9448c86f12e223818f
Oscar Urrutia <oscar.urrutia@amvsoluciones.com>
2023-03-21 12:07:58 +0100

    info card price size

--- src/components/InfoCardContainer/InfoCardContainer.tsx
+++ src/components/InfoCardContainer/InfoCardContainer.tsx
@@ -46,7 +46,7 @@ export const InfoCardContainer = ({productObject, setFrontImage, frontImage}:any
 
             <Box sx={{ width: { xl: "50%", xs: "90%" }, marginLeft: { xl: 10, xs: 2 }, marginRight: { xs: 1 }, direction: "flex", alignItems: { xs: "center" }, justifyContent: "center" }}>
                 <Typography sx={{ fontSize: { xl: 25, xs: 20 }, textAlign: { xs: "center" }, fontWeight: "bold", marginTop: "5%" }}>{productObject.productName}</Typography>
-                <Typography sx={{ fontSize: 15, fontWeight: "bold", textAlign: { xs: "center" }, marginBottom: 10, marginTop: { xs: 1 } }}>{productObject.price}</Typography>
+                <Typography sx={{ fontSize: 30, fontWeight: "bold", textAlign: { xs: "center" }, marginBottom: 10, marginTop: { xs: 1 } }}>{productObject.price}</Typography>
                 <Typography sx={{ fontSize: 15, fontWeight: "bold", marginLeft: 1, marginTop: "20%" }}>{`Color: ${productObject.color}`}</Typography>
                 <CardMedia component="img" src={productObject.image[0]} sx={{ border: "4px solid rgba(169, 182, 201, 0.2)", borderRadius: 5, cursor: "default", width: "15%", marginTop: 2 }} />
                 <Typography sx={{ fontSize: 15, fontWeight: "bold", marginTop: "20%" }}>DESCRIPCIÓN Y CARACTERÍSTICAS</Typography>
