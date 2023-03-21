665a7924b0f4a61460e5dce95177935d14515eb7
Oscar Urrutia <oscar.urrutia@amvsoluciones.com>
2023-03-21 14:43:40 +0100

    detalles

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,12 +1,14 @@
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, IconButton } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { useEffect, useState } from "react";
 import { CartContainer } from "../../components/CartContainer/CartContainer";
 import { useCustomDispatch, useCustomSelector } from "../../hooks/redux";
 import StoreIcon from '@mui/icons-material/Store';
 import { cleanCart } from "../../store/addToCarSlice/addToCarSlice";
-import { useNavigate } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import CancelIcon from '@mui/icons-material/Cancel';
+import { TopImageLogo } from "../../components/TopImageLogo/TopImageLogo";
+import { ExtraData } from "../../assets/mocks/ExtraData";
 
 interface Cart {
   id :number,
@@ -43,14 +45,15 @@ export const CartPage = ():JSX.Element=> {
 
   return (
     <Box sx={{display: 'flex',justifyContent:"center" ,alignItems:"center",flexDirection:"column",marginTop:{xl:10,xs:2}}}>
-      <StoreIcon sx={{color :"rgba(169, 182, 201, 0.6)",fontSize:80}}/>
+      <TopImageLogo logo={ExtraData.image}/>
+      <StoreIcon sx={{color :"rgba(169, 182, 201, 0.6)",fontSize:80,marginTop:"5%"}}/>
       <Typography sx={{fontSize:40, color :"rgba(169, 182, 201, 0.8)",borderBottom:"4px solid rgba(169, 182, 201, 0.8)",width:{xl:"35%",xs:"80%"},textAlign:"center"}}>Mi Cesta</Typography>
     {" "}
         <Grid container sx={{display: 'flex',flexDirection:"column",alignContent:"center"}}>
             {
             
               (cart.length === 0)
-              ? <Typography>La cesta está vacía</Typography>
+              ? <Typography fontSize={40}>La cesta está vacía</Typography>
               : cart?.map((cartProduct: any ,index:any ) => (<CartContainer  key={cartProduct.id} cartProduct={cartProduct}/>))
             
             
@@ -62,7 +65,7 @@ export const CartPage = ():JSX.Element=> {
         </Box>
         {cart.length?<Button variant="contained" sx={{marginBottom:2}} onClick={openModal}>Comprar</Button>:null}
         <Dialog open={isModalOpen} onClose={closeModal}>
-        <DialogTitle>Esta apunto de realizar una compra</DialogTitle>
+        <DialogTitle>Está apunto de realizar una compra</DialogTitle>
         <DialogContent>Confirme una acción para continuar</DialogContent>
         <DialogActions sx={{display:"flex",justifyContent:"center",paddingY:5}}>
         <Button color="error" variant="contained" onClick={closeModal}>
