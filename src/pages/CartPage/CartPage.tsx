e1be5b5ac426bd3e74c21511d8c4c9b1a764dc97
Oscar Urrutia <oscar.urrutia@amvsoluciones.com>
2023-03-21 13:22:12 +0100

    Condicionales

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -47,27 +47,30 @@ export const CartPage = ():JSX.Element=> {
       <Typography sx={{fontSize:40, color :"rgba(169, 182, 201, 0.8)",borderBottom:"4px solid rgba(169, 182, 201, 0.8)",width:{xl:"35%",xs:"80%"},textAlign:"center"}}>Mi Cesta</Typography>
     {" "}
         <Grid container sx={{display: 'flex',flexDirection:"column",alignContent:"center"}}>
-            {cart?.map((cartProduct: any ,index:any ) => (
-              
-            <CartContainer  key={cartProduct.id} cartProduct={cartProduct} />
+            {
             
-            ))}
+              (cart.length === 0)
+              ? <Typography>La cesta está vacía</Typography>
+              : cart?.map((cartProduct: any ,index:any ) => (<CartContainer  key={cartProduct.id} cartProduct={cartProduct}/>))
+            
+            
+            }
       </Grid>
         <Box sx={{display: 'flex',justifyContent:"space-between",width:{xl:"35%",xs:"80%"},marginBottom:5}}>
           <Typography sx={{fontSize:25}}>Total: </Typography>
           <Typography sx={{fontSize:25}}>{`${totalToPay()} €`}</Typography>
         </Box>
-        <Button variant="contained" sx={{marginBottom:2}} onClick={openModal}>Comprar</Button>
+        {cart.length?<Button variant="contained" sx={{marginBottom:2}} onClick={openModal}>Comprar</Button>:null}
         <Dialog open={isModalOpen} onClose={closeModal}>
         <DialogTitle>Esta apunto de realizar una compra</DialogTitle>
         <DialogContent>Confirme una acción para continuar</DialogContent>
         <DialogActions sx={{display:"flex",justifyContent:"center",paddingY:5}}>
+        <Button color="error" variant="contained" onClick={closeModal}>
+            Cancelar
+        </Button>
         <Button color="success" variant="contained" onClick={handleClick}>
             Comprar
-          </Button>
-          <Button color="error" variant="contained" onClick={closeModal}>
-            Cancelar
-          </Button>
+        </Button>
         </DialogActions>
       </Dialog>
     </Box>
