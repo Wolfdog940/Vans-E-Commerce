b6bd395690d60f4a58109adce9b8d832da1af7b5
Oscar Urrutia <oscarurrutia940@gmail.com>
2023-10-01 20:43:22 +0200

    marginTop infoCartPage

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -69,7 +69,7 @@ export const ProductCard = ({product}:any):JSX.Element => {
 
     return (
         <Grid item xl={3}  xs={12} lg={4} sm={6} sx={{display:"flex",justifyContent:"center"}} >
-            <Card  sx={{width:"95%",maxHeight:"100%", margin:2,marginTop:{xs:"20%"},background:"rgba(169, 182, 201, 0.2)",borderRadius:3}}>
+            <Card  sx={{width:"95%",maxHeight:"100%", margin:2,background:"rgba(169, 182, 201, 0.2)",borderRadius:3}}>
                 <Box sx={{display:"flex",justifyContent:"center" }}>
                     <CardMedia   sx={{width:"95%",minHeight:{xl:"100%",lg:"80%",sm:"90%",xs:"90%"}}} >
                         <Link to={`/infoPage/${productId}`}>
