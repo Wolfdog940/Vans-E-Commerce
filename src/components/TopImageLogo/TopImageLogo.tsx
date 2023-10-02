7cb204fa5d1dfe4229063ff9b29f72b6565fd2a9
Oscar Urrutia <oscarurrutia940@gmail.com>
2023-10-02 14:41:54 +0200

    error vercel

--- src/components/TopImageLogo/TopImageLogo.tsx
+++ src/components/TopImageLogo/TopImageLogo.tsx
@@ -4,9 +4,9 @@ import Box from '@mui/material/Box';
 import { AppBar, Badge, IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useCustomSelector } from "../../hooks/redux";
-import { Filter } from "../Filter/Filter"
+import { Filter } from "../Filter/filter"
 import { useLocation } from 'react-router-dom'
-import { RedirectToHomeButton } from "../RedirectToHomeBotton/RedirectToHomeButton";
+import { RedirectToHomeButton } from "../RedirectToHomeBotton/redirectToHomeButton";
 import { styles } from "./styles";
 
 interface Image{
