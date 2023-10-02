84b84b2bcccd51710f717838c4c2ca8f4b388006
Oscar Urrutia <oscarurrutia940@gmail.com>
2023-10-02 17:33:01 +0200

    pruebas 2

---
+++ src/components/Filter/styles.tsx
@@ -0,0 +1,14 @@
+type Styles = {
+    [key: string]: {};
+};
+
+export const filterStyles:Styles = {
+    filterIcon :{
+        color:"blue",
+        fontSize:40
+    },
+    menuStyles:{
+        marginTop:"5%"
+    },
+    
+}
